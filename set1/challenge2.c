e575c09b5d1a4d4e6f82749db4ec82f16fece15c
Sebastien Ronsse <sronsse@gmail.com>
2015-03-11 11:27:34 -0700

    Committed challenge 2.

---
+++ set1/challenge2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <hex.h>
+#include <xor.h>
+
+void challenge2(char *hex, char *xor)
+{
+	uint8_t *hex_bytes;
+	uint8_t *xor_bytes;
+	uint8_t *result;
+	int hex_len;
+	int xor_len;
+	int result_len;
+	int i;
+
+	printf("*** Challenge 2: Fixed XOR\n");
+
+	/* Print input hex string */
+	printf("Input hex: %s\n", hex);
+	printf("Input xor: %s\n", xor);
+
+	/* Convert input hex string to byte array */
+	hex_bytes = hex_decode(hex, &hex_len);
+
+	/* Convert input xor string to byte array */
+	xor_bytes = hex_decode(xor, &xor_len);
+
+	/* Set result length */
+	result_len = hex_len;
+
+	/* Print ASCII xor string representation */
+	printf("ASCII (xor input): %s\n", xor_bytes);
+
+	/* XOR encrypt data */
+	result = xor_encrypt(hex_bytes, xor_bytes, result_len);
+
+	/* Print out result */
+	printf("Result: ");
+	for (i = 0; i < hex_len; i++)
+		printf("%02x", result[i]);
+	printf("\n");
+
+	/* Free arrays */
+	free(hex_bytes);
+	free(xor_bytes);
+	free(result);
+}
+
--- set1/set1.c
+++ set1/set1.c
@@ -1,8 +1,10 @@
 extern void challenge1(char *str);
+extern void challenge2(char *hex, char *xor);
 
 void set1()
 {
 	/* Run all challenges */
 	challenge1("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+	challenge2("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965");
 }
 
